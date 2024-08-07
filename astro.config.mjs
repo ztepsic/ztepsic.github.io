aac9715368d8d1563443492e42e5deb13218cd6d
Zeljko Tepsic <ztepsic@gmail.com>
2024-08-07 16:51:47 +0200

    Added basic info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.ztepsic.com'
+  site: "https://www.ztepsic.com",
+  integrations: [icon()],
 });
