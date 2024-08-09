3dad8b69a316d38d07316d8cf9fc12debdaf8cc1
Zeljko Tepsic <ztepsic@gmail.com>
2024-08-09 12:37:49 +0200

    Added i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,17 @@ export default defineConfig({
     icon(),
     sitemap({
       lastmod: new Date("2024-08-07"),
+      i18n: {
+        defaultLocale: "en", // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: "en-US", // The `defaultLocale` value must present in `locales` keys
+          hr: "hr-HR",
+        },
+      },
     }),
   ],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "hr"],
+  },
 });
