17b227652ef92be6baba7155b38816e577d583e2
Zeljko Tepsic <ztepsic@gmail.com>
2024-08-09 11:42:42 +0200

    Added robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.ztepsic.com",
-  integrations: [icon()],
+  integrations: [
+    icon(),
+    sitemap({
+      lastmod: new Date("2024-08-07"),
+    }),
+  ],
 });
